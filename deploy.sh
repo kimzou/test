c0325099c829161740a3e691f170304571878695
Alex <alexandra.nguyen@epitech.eu>
2020-11-26 14:34:01 +0100

    travis & deploy

---
+++ deploy.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#ssh connection
+
+
+#deploy on dev server
+git remote add deploy ssh://$ADDRESS/test
+git push deploy dev
+
+#deploy on prod server
+git checkout prod
+git merge dev
+
+
