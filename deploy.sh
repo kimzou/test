d29aba29ed9603ff1c30f0fb91df19f40c350658
Alex <alexandra.nguyen@epitech.eu>
2020-11-27 15:28:06 +0100

    update

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,23 @@
 #!/bin/bash
 
-ADDRESS="ubuntu@ec2-35-181-29-4.eu-west-3.compute.amazonaws.com"
+#ADDRESS="ubuntu@ec2-35-181-29-4.eu-west-3.compute.amazonaws.com"
 PRODUCTION="prod"
 
 #ssh connection
-ssh -i ~/.ssh/selfserver.pem $ADDRESS
+#ssh -i ~/.ssh/selfserver.pem $ADDRESS
+
+eval "$(ssh-agent -s)" # Start ssh-agent cache
+chmod 600 .travis/selfserver.pem # Allow read access to the private key
+ssh-add .travis/selfserver.pem # Add the private key to SSH
+
+#git config --global push.default matching
+git remote add deploy ssh://$SEVER_ADDRESS/$DIR
+git pull deploy dev
+
 
 #deploy on prod server
-git remote add deploy ssh://$ADDRESS/test
-git push origin $PRODUCTION
+#git remote add deploy ssh://$ADDRESS/$DIR
+#git push origin $PRODUCTION
 
 #pull on dev server
 #git pull deploy dev 
