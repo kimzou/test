54dfeb80991753692ce783f17ffce22ca8877dbc
Alex <alexandra.nguyen@epitech.eu>
2020-11-27 10:36:41 +0100

    ssh co

--- deploy.sh
+++ deploy.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 
+ADDRESS="ubuntu@ec2-35-181-29-4.eu-west-3.compute.amazonaws.com"
+PRODUCTION="prod"
 
 #ssh connection
-
-
-#deploy on dev server
-#git remote add deploy ssh://$ADDRESS/test
-#git push deploy dev
+ssh -i ~/.ssh/selfserver.pem $ADDRESS
 
 #deploy on prod server
-#git checkout prod
-#git pull deploy 
+git remote add deploy ssh://$ADDRESS/test
+git push origin $PRODUCTION
 
-git fetch --depth=1 git@github.com:kimzou/test.git refs/heads/*:refs/remotes/origin/*
-git checkout origin prod
-git merge dev
-git push origin prod
+#pull on dev server
+#git pull deploy dev 
+
+#git fetch --depth=1 git@github.com:kimzou/test.git refs/heads/*:refs/remotes/origin/*
+#git checkout origin prod
+#git merge dev
+#git push origin prod
 
