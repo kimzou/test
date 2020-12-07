6024d213ac526029a89fb91ad7482df0a7267026
Alex <alexandra.nguyen@epitech.eu>
2020-12-07 16:01:32 +0100

    checkout origin

--- deploy.sh
+++ deploy.sh
@@ -24,7 +24,7 @@ echo "After add remote & before pull"
 
 #git push origin HEAD:prod
 #git push git@github.com:kimzou/test.git HEAD:prod
-git checkout prod
+git checkout origin prod
 git push origin prod
 
 echo "After git pull"
